a7b4a43f12165c5c1911c0a53e10afca11173215
audiotore341 <102002229+audiotore341@users.noreply.github.com>
2022-03-21 10:39:16 -0700

    Add files via upload

---
+++ ToreOS/source code/Base.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+/* This is the base os for functionality, pretty much the core
+ * of the operating system! */
+
+namespace OS
+{
+    class Base
+    {
+        Commands cmd_func = new Commands();
+        public void run()
+        {
+            Console.WriteLine("Copyright (c) ToreOS 1.0 2022");
+            Console.WriteLine("Type 'help' to get a list of commands!");
+
+            bool running = true;
+            while (running == true)
+            {
+                Console.Write("[<Z>]:");
+                string input = Console.ReadLine();
+
+                if (input == "help")
+                {
+                    cmd_func.Help();
+                }
+
+                if (input == "system_shutdown")
+                {
+                    Console.Clear();
+                    cmd_func.system_shutdown();
+                    break;
+                }
+
+                if (input == "system_reboot")
+                {
+                    Console.Clear();
+                    cmd_func.system_reboot();
+                }
+
+                if (input == "system_about")
+                {
+                    cmd_func.system_about();
+                }
+
+                if (input == "open_notepad")
+                {
+                    cmd_func.open_notepad();
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ ToreOS/source code/Bootloader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+/* This is for the bootloader 
+ * Please be carefull on what code you make! */
+
+namespace OS
+{
+    public class Bootloader
+    {
+        static Base running = new Base();
+        static void Main(string[] args)
+        {          
+            Console.WriteLine("Booting ToreOS 1.0.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Booting ToreOS 1.0..");
+            Thread.Sleep(1000);
+            Console.WriteLine("Booting ToreOS 1.0...");
+            Console.WriteLine("OS has successfully booted!");
+            Thread.Sleep(1000);
+            if (File.Exists("username.data") & File.Exists("password.data"))
+            {
+                running.run();
+            }
+            else
+            {             
+                Console.Write("Enter a username: ");
+                string register_username = Console.ReadLine();
+                using (StreamWriter write_username = new StreamWriter(@"username.data"))
+                {
+                    write_username.WriteLine(register_username);
+                }
+                Console.Write("Enter a password: ");
+                string register_password = Console.ReadLine();
+                using (StreamWriter write_password = new StreamWriter(@"password.data"))
+                {
+                    write_password.WriteLine(register_password);
+                }
+                Console.WriteLine("username, and password has been created!");
+                Thread.Sleep(1000);
+                running.run();
+                
+
+            }
+            
+           
+            
+            
+            
+        }
+
+        public void reboot_101()
+        {
+            Console.WriteLine("Rebooting ToreOS...");
+            Thread.Sleep(3000);
+            Console.WriteLine("OS has successfully rebooted!");
+            Thread.Sleep(1000);
+            running.run();
+        }
+    }
+}
