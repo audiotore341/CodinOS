b637f164c543a3317a25cd9ddee92c16079c3428
audiotore341 <102002229+audiotore341@users.noreply.github.com>
2022-03-26 21:43:49 -0500

    Add files via upload

---
+++ CodinOS Source code/Bootloader.cs
@@ -0,0 +1,100 @@
+﻿/* This is currently a beta version of CodinOS.
+ * CodinOS is not an actual OS. It's just a os simulator, and
+ * made for programmers who want to get an picture of what it's like
+ * to make a actual programing language. */
+
+// This is the bootloader, and setup for new users
+
+using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace OS
+{
+    public class Bootloader
+    {
+        public static Base bn = new Base();
+        public static void Main(string[] args)
+        {
+            // Fake loading screen.
+            Console.WriteLine("Booting CodinOS.");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Booting CodinOS..");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Booting CodinOS...");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            // host os checker.
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Console.WriteLine("Sorry, but your platforum is not compatible!");
+            }
+            else
+            {
+                if (File.Exists(@"user/username.data") && File.Exists(@"user/password.data"))
+                {
+                    // Reads the user's username
+                    using (StreamReader read_username = new StreamReader(@"user/username.data"))
+                    {
+                        string read_username1;
+                        read_username1 = read_username.ReadLine();
+                        while (true)
+                        {
+                            Console.WriteLine("username: " + read_username1);
+                            Console.Write("password: ");
+                            string verify_password = Console.ReadLine();
+                            // Reads the password.
+                            using (StreamReader read_password = new StreamReader(@"user/password.data"))
+                            {
+                                string read_password1;
+                                read_password1 = read_password.ReadLine();
+                                if (verify_password == read_password1)
+                                {
+                                    bn.run(null);
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Incorrect username or password, please try again!");
+                                }
+                            
+                            }
+                        }
+                    }
+                    
+                        
+                    
+                }
+                else
+                {
+                    Console.Write("Would you like to have a user and pass? [yes/no]: ");
+                    string user = Console.ReadLine();
+                    if (user == "yes")
+                    {
+                        Console.Write("Enter a username: ");
+                        string username = Console.ReadLine();
+                        using (StreamWriter write_username = new StreamWriter(@"user\username.data"))
+                        {
+                            write_username.WriteLine(username);
+                        }
+                        Console.Write("Enter a password: ");
+                        string password = Console.ReadLine();
+                        using (StreamWriter write_password = new StreamWriter(@"user\password.data"))
+                        {
+                            write_password.WriteLine(password);
+                        }
+                        Console.WriteLine("Done!");
+                        bn.run(null);
+                        
+
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CodinOS Source code/Commands.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+/* This is where all the command functionality is. To add a command, make a function with the command name.
+ * then on the base.cs add a if statement for when the user types the command name */
+
+namespace OS
+{
+    class Commands
+    {
+        public static Bootloader reboot = new Bootloader();
+        static Base bs = new Base(); // Used for system reboot, shutdown commands.
+        public static void help()
+        {
+            Console.WriteLine("List of commands: 'help', system_shutdown, system_reboot");
+        }
+
+        public static void system_shutdown()
+        {
+            
+            Console.Write("Are you sure? [yes/no]: ");
+            string shutdown = Console.ReadLine();
+            if (shutdown == "yes")
+            {
+                Console.Clear();
+                Console.WriteLine("Shutting down...");
+                Thread.Sleep(1000);
+                Environment.Exit(0); // Closes the program.
+            }
+            else
+            {
+                Console.WriteLine("Canceling...");
+                bs.run(null);
+
+                
+            }
+        }
+        public static void system_reboot()
+        {
+            Console.Write("Are you sure? [yes/no]: ");
+            string reboot = Console.ReadLine();
+            if (reboot == "yes")
+            {
+                Bootloader.Main(null);
+
+            }
+            else
+            {
+                Console.WriteLine("Canceling...");
+                bs.run(null);
+            }
+        }
+
+
+
+    }
+}
